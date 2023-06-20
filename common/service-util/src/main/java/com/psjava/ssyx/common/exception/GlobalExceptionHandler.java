43abf2412af88361b7d3dbcf91b8a6fec0208177
PShawn <pshawn@126.com>
2023-06-21 00:25:24 +0800

    统一异常处理类

---
+++ common/service-util/src/main/java/com/psjava/ssyx/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.psjava.ssyx.common.exception;
+
+import com.psjava.ssyx.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class) //异常处理器
+    @ResponseBody //返回json数据
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail(null);
+    }
+
+    /**
+     * 自定义异常处理方法
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(SsyxException.class)
+    @ResponseBody
+    public Result error(SsyxException e){
+        return Result.fail(null);
+    }
+}

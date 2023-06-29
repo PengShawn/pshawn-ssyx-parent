b1317bc04eb8fa792512cce517e67e3403b318cc
PShawn <pshawn@126.com>
2023-06-29 12:07:54 +0800

    springcloud nacos

--- service/service-sys/src/main/java/com/psjava/ssyx/ServiceSysApplication.java
+++ service/service-sys/src/main/java/com/psjava/ssyx/ServiceSysApplication.java
@@ -2,9 +2,11 @@ package com.psjava.ssyx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //区域管理模块启动类
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceSysApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceSysApplication.class, args);
